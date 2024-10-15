c9b4b24070608e7a3d14d16ba4689f402e910062
kabirslack <kabirslack@gmail.com>
2024-10-15 22:49:48 +0600

    update

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-import getBooks from "../lib/getBooks.js";
 import bookTemplate from "../lib/bookTemplate.js";
 import loader from "../lib/loader.js";
 import { BooksLocal } from "./lib/books.js";
 import DisplayWishlist from "./lib/displayWishlist.js";
+import getBooks from "./lib/getBooks.js";
+import { RegisterPaginateEvent, RenderPaginate } from "./lib/RenderPaginate.js";
 export const books = [];
-const main = async () => {
+const paginate = {
+  nextPage: "https://gutendex.com/books",
+  previousPage: null,
+};
+export const main = async (url) => {
   try {
     const input = document.getElementById("search-input");
     const bookList = document.getElementById("book-list");
 
     loader(bookList, true);
-    const booksAll = BooksLocal.results;
+    // const booksAll = BooksLocal.results;
+    console.log("get books now");
+    // const booksAll = await getBooks;
+
+    const response = await fetch(url);
+    const data = await response.json();
+    const booksAll = data.results;
+    paginate.nextPage = data.next;
+    paginate.previousPage = data.previous;
+
     loader(bookList, false);
     books.push(...booksAll);
 
@@ -24,18 +38,31 @@ const main = async () => {
 
     render(bookList, books);
   } catch (error) {
+    console.log(error.message);
   } finally {
     DisplayWishlist();
+    RenderPaginate(paginate);
+    RegisterPaginateEvent();
   }
 };
 
-main();
+main(paginate.nextPage);
 
-function render(location, books) {
+export function render(location, books) {
   console.log("render called");
   location.innerHTML = "";
   books.forEach((book) => {
     let html = bookTemplate(book);
     location.innerHTML += html;
   });
+}
+
+export function handleNext() {
+  console.log("handle next called");
+  main(paginate.nextPage);
+}
+
+export function handlePrevious() {
+  console.log("handle previous called");
+  main(paginate.previousPage);
 }
