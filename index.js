8c2fbc7ffc3e934de946f0906425d31297503b6d
kabir <kabirslack@gmail.com>
2024-10-17 17:27:43 +0600

    update

--- index.js
+++ index.js
@@ -1,64 +1,7 @@
 import loader from "../lib/loader.js";
-import { BooksLocal } from "./lib/books.js";
+
 import BookDesign from "./lib/bookTemplate.js";
 import { RegisterPaginateEvent, RenderPaginate } from "./lib/RenderPaginate.js";
-// import { BooksLocal } from "./lib/books.js";
-// import DisplayWishlist from "./lib/displayWishlist.js";
-// import getBooks from "./lib/getBooks.js";
-// import { RegisterPaginateEvent, RenderPaginate } from "./lib/RenderPaginate.js";
-// export const books = [];
-// const paginate = {
-//   nextPage: "https://gutendex.com/books",
-//   previousPage: null,
-// };
-// export const main = async (url) => {
-//   try {
-//     const input = document.getElementById("search-input");
-//     const bookList = document.getElementById("book-list");
-
-//     loader(bookList, true);
-//     // const booksAll = BooksLocal.results;
-//     console.log("get books now");
-//     // const booksAll = await getBooks;
-
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     const booksAll = data.results;
-//     paginate.nextPage = data.next;
-//     paginate.previousPage = data.previous;
-
-//     loader(bookList, false);
-//     books.length=0;
-//     books.push(...booksAll);
-
-//     input.addEventListener("keyup", (e) => {
-//       let filteredBooks = books.filter((book) => {
-//         return book.title.toLowerCase().includes(e.target.value.toLowerCase());
-//       });
-//       console.log(filteredBooks);
-//       render(bookList, filteredBooks);
-//     });
-
-//     render(bookList, books);
-//   } catch (error) {
-//     console.log(error.message);
-//   } finally {
-//     DisplayWishlist();
-//     RenderPaginate(paginate);
-//     RegisterPaginateEvent();
-//   }
-// };
-
-// main(paginate.nextPage);
-
-// export function render(location, books) {
-//   console.log("render called");
-//   location.innerHTML = "";
-//   books.forEach((book) => {
-//     let html = bookTemplate(book);
-//     location.innerHTML += html;
-//   });
-// }
 
 let page;
 let result;
@@ -206,6 +149,7 @@ async function homepage() {
 
   document.getElementById("filter").addEventListener("keyup", (event) => {
     let title = event.target.value;
+    localStorage.setItem("filter", title);
     if (filteredBooks) {
       let book = filteredBooks.filter((item) =>
         item.title.toLowerCase().includes(title.toLowerCase())
@@ -215,6 +159,7 @@ async function homepage() {
   });
   document.getElementById("topic").addEventListener("change", async (event) => {
     let topic = event.target.value;
+    localStorage.setItem("topic", topic);
     if (topic === "all") {
       RenderBooks(booksdata);
       return;
@@ -237,6 +182,20 @@ async function homepage() {
   filteredBooks = booksdata;
   let hash = window.location.hash;
   if (hash === "") {
+    let topic = localStorage.getItem("topic");
+    let filter = localStorage.getItem("filter");
+
+    // check if filter an topic exist
+    if (topic && filter) {
+      filteredBooks = booksdata.filter((item) => {
+        return (
+          item.bookshelves.join(",").includes(topic) &&
+          item.title.toLowerCase().includes(filter.toLowerCase())
+        );
+      });
+      RenderBooks(filteredBooks);
+    }
+  } else {
     RenderBooks(result?.results);
   }
   toggleCount();
